27b496ed4bf7de1f634f76f013b20896da9e38c7
Mozin Omer <mozinomer12@gmail.com>
2019-05-29 05:13:20 +0500

    homepage section 2 and 3 ready

--- header.php
+++ header.php
@@ -2,33 +2,9 @@
 <html>
 <head>
 	<title>asd</title>
-	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-	<script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600i,700&display=swap" rel="stylesheet">
 </head>
 <?php wp_head();?>
 <body>
 
-<div class="main_content">
-asda
-<section class="section_1">
-	<div class="row">
-		<div class="col-md-8">
-			
-		</div>
-		<div class="col-md-4">
-			<div class="row menu_row">
-				<?php wp_nav_menu( array( 'theme_location' => 'my-custom-menu' ) ); ?>
-			</div>
-			<div class="row services_page">
-				<?php $data_query =  new WP_Query(array('post_type' => 'services', 'order_by'=>'Des', 'posts_per_page' => 5));?>
-				<?php while ( $data_query->have_posts() ) : $data_query->the_post();?>
-
-					<span class="services_content">
-						<h1><?php the_title(); ?></h1>
-						<span class="excerpt_services"><?php the_excerpt(); ?></span>
-					</span>
-				<?php endwhile ?>
-			</div>
-		</div>
-	</div>
-</section>
+<div class="main_container">
---
+++ page-home.php
@@ -0,0 +1,132 @@
+<?php
+
+
+/**
+ *
+ * Template Name: Home Template 1
+ *
+ */
+get_header();?>
+
+
+<div class="section_2_home_page">
+	<div class="container">
+		<div class="block_right">
+			<div class="display_flex">
+				<h2>
+					Our recent Cases
+				</h2>
+			</div>
+		</div>
+		<div class="left_block">
+			<div class="container">
+				<div class="row">
+					<h3 class="main_heading_left_block_section_2--home_page">
+						London-based ride sharing solution
+					</h3>
+				</div>
+				<div class="row">
+					<div class="col-md-6">
+						<span class="heading_block_left_section-2--home_page">
+							backend + api
+						</span>
+						<p class="description_left_block_section_2--home_page">
+							 Laravel backend with an API that makes it easy to create smart contracts on demand as well as access and manage all date stored on the database.
+						</p>
+					</div>
+					<div class="col-md-6">
+						<div class="row">
+							<span class="heading_block_left_section-2--home_page">
+								Web App
+							</span>
+							<p class="description_left_block_section_2--home_page">
+								 A clean web interface with full control of the database and all the service features.
+							</p>
+						</div>
+						<div class="row">
+							<span class="heading_block_left_section-2--home_page">
+								iOS & Andoid App
+							</span>
+							<p class="description_left_block_section_2--home_page">
+								 An app working with the API and geolocation services.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="botton_text_section_2--home_page">
+			<div class="row display_flex">
+				<div class="col-md-9">
+					<p class="description_bottom_text_section_2--home_page">
+						 Mega Transfers enables you to instantly compare cab quotes and book long distance & local trips across London, on website, mobile and tablet. Given the inconvenience and ever higher prices of public transport in city, it's often cheaper and easier for friends, family or colleagues to travel anywhere in the London by Cab. So now, if you need to get to & from the airport, hotel, concert, meeting or any occasion, you can book a ride, London wide, with Mega Transfers.
+					</p>
+				</div>
+				<div class="col-md-3">
+					<a class="view_more_button_section_2--home_page" href="#">view</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="section_3--home_page">
+	<div class="container">
+		<div class="row display_flex align-item-top">
+			<div class="col-md-6 col-md-6-with-padding">
+				<h2 class="heading_section_3">
+					UI/UX, Frontend & Backend For E-Commerce
+				</h2>
+				<div class="row display_flex">
+					<div class="col-md-9">
+						<p class="description_bottom_text_section_2--home_page">
+							Phuppi.com has expertise which helps offer their customers the same quality and value-for-money. Phuppi.com was launched in 2018. Since then, Phuppi.com is growing its market share all over. Get quality goods with their fantastic collection of household items that add to their success every day.
+						</p>
+					</div>
+					<div class="col-md-3">
+						<a class="view_more_button_section_2--home_page" href="#">view</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6 col-md-6-with-padding">
+				<div class="row">
+					<h3 class="heaidng_right_section_3_section-3--home_page">
+						Backend + API
+					</h3>
+					<p>
+						 Villaex Technologies developed it using Magento and well-composed database following provided specifications as well as a fast and convenient API that allows for a smooth connection to the database.
+					</p>
+				</div>
+				<div class="row">
+					<div class="col-md-6">
+						<h3 class="heaidng_right_section_3_section-3--home_page">
+							 Web App
+						</h3>
+						<p>
+							 User friendly web interface that allows you one-click secure checkout, enables customers to track orders.
+						</p>
+					</div>
+					<div class="col-md-6">
+						<h3 class="heaidng_right_section_3_section-3--home_page">
+							 iOS & Andoid App
+						</h3>
+						<p>
+							 A clean web interface with full control of the database and all the service features.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<img src="#">
+		</div>
+	</div>
+</div>
+
+
+
+
+
+
+
+<?php get_footer();?>
