7256c1f77d9715d2f6e4681ee741c9e88992605f
Mozin Omer <mozinomer12@gmail.com>
2019-07-16 13:16:28 +0500

    making a blog page header

--- page-blog.php
+++ page-blog.php
@@ -4,67 +4,106 @@
 		categies conaiener
 	</div>
 </div>
-
-
-<h2>Vertical Tabs</h2>
-<p>Click on the buttons inside the tabbed menu:</p>
-<?php
-
-// WP_Query arguments
-$args = array (
-	'post_type'              => array( 'post' ),
-	'post_status'            => array( 'publish' ),
-	'nopaging'               => true,
-	'order'                  => 'DSC',
-	'orderby'                => 'menu_order',
-);
-
-// The Query
-$services = new WP_Query( $args );
-?>
-// The Loop
-<ul class="nav nav-tabs" id="myTab" role="tablist">
+<div class="congtainer_main_tabs-blog">
 	<?php
-	if ( $services->have_posts() ) {
-		while ( $services->have_posts() ) {
-			$services->the_post(); ?>
-		  <li class="nav-item">
-		    <a class="nav-link active" data-toggle="tab" href="#<?php echo get_the_ID();?>" role="tab" aria-controls="home"><?php the_title(); ?></a>
-		  </li>
-	<?php } ?>
-</ul>
 
-<div class="tab-content">
+	// WP_Query arguments
+	$args = array (
+		'post_type'              => array( 'post' ),
+		'post_status'            => array( 'publish' ),
+		'nopaging'               => true,
+		'order'                  => 'DSC',
+		'orderby'                => 'menu_order',
+	);
+
+	// The Query
+	$services = new WP_Query( $args );
+	?>
+	<ul class="nav nav-tabs" id="myTab" role="tablist">
+		<?php
+		if ( $services->have_posts() ) {
+			while ( $services->have_posts() ) {
+				$services->the_post(); ?>
+			  <li class="nav-item">
+			    <a class="nav-link" data-toggle="tab" href="#<?php echo get_the_ID();?>" role="tab"><?php the_title(); ?></a>
+			  </li>
+		<?php } ?>
+	</ul>
+
+	<div class="tab-content">
+		<?php
+			// WP_Query arguments
+			$args = array (
+				'post_type'              => array( 'post' ),
+				'post_status'            => array( 'publish' ),
+				'nopaging'               => true,
+				'order'                  => 'DSC',
+				'orderby'                => 'menu_order',
+			);
+
+			// The Query
+			$services = new WP_Query( $args );
+		if ( $services->have_posts() ) {
+			while ( $services->have_posts() ) {
+				$services->the_post(); ?>
+				  <div class="tab-pane" id="<?php echo get_the_ID();?>" role="tabpanel">
+				  	<?php the_title(); ?>
+				  </div>
+	  <?php } ?>
 	<?php
-		// WP_Query arguments
-		$args = array (
-			'post_type'              => array( 'post' ),
-			'post_status'            => array( 'publish' ),
-			'nopaging'               => true,
-			'order'                  => 'DSC',
-			'orderby'                => 'menu_order',
-		);
+		} ?>
+	</div>
 
-		// The Query
-		$services = new WP_Query( $args );
-	if ( $services->have_posts() ) {
-		while ( $services->have_posts() ) {
-			$services->the_post(); ?>
-  <div class="tab-pane active" id="<?php echo get_the_ID();?>" role="tabpanel">
+	<?php
+		}
+	else {
+		// no posts found
+	}
 
-  </div>
-  <?php } ?>
+	// Restore original Post Data
+	wp_reset_postdata();
+
+	?>
 </div>
 
-<?php
-	}
-else {
-	// no posts found
-}
 
-// Restore original Post Data
-wp_reset_postdata();
 
-?>
+
+<div class="row">
+	<div class="col-md-7">
+		<div class="row">
+			<div class="col-md-4">
+				latest article
+			</div>
+			<div class="col-md-4">
+				popular article
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-4">
+				<?php the_post_thumbnail(); ?>
+			</div>
+			<div class="col-md-5">
+				<div class="row">
+					news -> marketing
+				</div>
+				<div class="row">
+					<?php the_title(); ?>
+				</div>
+				<div class="row">
+					<?php the_excerpt(); ?>
+				</div>
+				<span class="timeadde">
+					16 minutes ago
+				</span>
+			</div>
+			<div class="col-md-3">
+				<span class="author_name_post-blog">
+					<?php the_author(); ?>
+				</span>
+			</div>
+		</div>
+	</div>
+</div>
 
 <?php get_footer();?>
