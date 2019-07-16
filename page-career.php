e94697f1597dfb8a39956df7764c3e26da8d8bce
Mozin Omer <mozinomer12@gmail.com>
2019-07-16 16:13:20 +0500

    making a blog page header

--- page-career.php
+++ page-career.php
@@ -3,22 +3,39 @@
 ?>
 
 <div class="row_main_bg_career display_flex" style="background: url('<?php echo get_the_post_thumbnail_url(); ?>');">
-	<div class="container">
+	<div class="container heading_container_main_career_main">
 		 <h3 class="heading_container_main_career">
 			 <?php the_field('excerpt'); ?>
 		 </h3>
+		 <p class="text-center">
+		 	join our team
+		 </p>
 	</div>
 </div>
 <div class="row">
 	<div class="container">
-		<h2>
+		<h2 class="text-center">
 			Avaialable <?php the_title(); ?>
 		</h2>
         <?php if( have_rows('avaiable_career') ): ?>
-            <ul>
+            <ul class="career_list_container">
             <?php while( have_rows('avaiable_career') ): the_row(); ?>
                 <li>
-                    <a href="#"><?php the_sub_field('avaiable_careers'); ?></a>
+                	<div class="row">
+                		<div class="col-md-8">
+		                    <a href="#"><?php the_sub_field('avaiable_careers'); ?></a>
+                		</div>
+                		<div class="col-md-4">
+                			<div class="row">
+                				<div class="col-md-6">
+                					<a href="#">view Detail</a>
+                				</div>
+                				<div class="col-md-6">
+                					<a href="#">Apply</a>
+                				</div>
+                			</div>
+                		</div>
+                	</div>
                 </li>
             <?php endwhile; ?>
             </ul>
