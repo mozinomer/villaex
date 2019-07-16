d744571e1b716b59969fe12ceea231585f73bfd8
Mozin Omer <mozinomer12@gmail.com>
2019-07-16 13:47:30 +0500

    making a blog page header

--- page-blog.php
+++ page-blog.php
@@ -49,8 +49,8 @@
 				  <div class="tab-pane" id="<?php echo get_the_ID();?>" role="tabpanel">
 				  	<?php the_title(); ?>
 				  </div>
-	  <?php } ?>
-	<?php
+	  <?php }
+
 		} ?>
 	</div>
 
@@ -70,7 +70,7 @@
 
 
 <div class="row">
-	<div class="col-md-7">
+	<div class="col-md-8">
 		<div class="row">
 			<div class="col-md-4">
 				latest article
@@ -79,30 +79,48 @@
 				popular article
 			</div>
 		</div>
-		<div class="row">
-			<div class="col-md-4">
-				<?php the_post_thumbnail(); ?>
+		<?php
+			$args = array (
+				'post_type'              => array( 'post' ),
+				'post_status'            => array( 'publish' ),
+				'nopaging'               => true,
+				'order'                  => 'DSC',
+				'orderby'                => 'menu_order',
+			);
+			$services = new WP_Query( $args );
+			if ( $services->have_posts() ) {
+			while ( $services->have_posts() ) {
+					$services->the_post();
+		?>
+		<div class="row row_contains_blog">
+			<div class="col-md-3 the_post_thumbnail_blog">
+				<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
 			</div>
 			<div class="col-md-5">
-				<div class="row">
+				<div class="row breaducrumb_blog_post">
 					news -> marketing
 				</div>
-				<div class="row">
-					<?php the_title(); ?>
+				<div class="row the_title_blog_post">
+					<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
 				</div>
-				<div class="row">
+				<div class="row the_excerpt_blog_post">
 					<?php the_excerpt(); ?>
 				</div>
 				<span class="timeadde">
-					16 minutes ago
+					<?php the_date(); ?>
 				</span>
 			</div>
-			<div class="col-md-3">
+			<div class="col-md-3 text-center">
 				<span class="author_name_post-blog">
+					<span class="image_container_author"></span>
 					<?php the_author(); ?>
 				</span>
 			</div>
 		</div>
+
+		  <?php }
+
+			} ?>
 	</div>
 </div>
 
