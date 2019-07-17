4eae6ddc2e1124c49160177801010ae76a3cff0a
Mozin Omer <mozinomer12@gmail.com>
2019-07-17 16:28:50 +0500

    form starting

---
+++ page-career-form.php
@@ -0,0 +1,39 @@
+<?php get_header(); ?>
+
+
+
+
+
+<div class="row top_row_container">
+	<div class="container">
+		<h3>
+			Social Media Coordinator
+		</h3>
+		<p>
+			new york and united states social media container
+		</p>
+	</div>
+</div>
+
+
+
+
+
+
+
+<div class="row from-row_container">
+	<div class="container">
+		<div class="row">
+			<h2>
+				personal Information
+			</h2>
+		</div>
+		<div class="row">
+			<div class="container contaienr_ormf">
+				<?php echo do_shortcode('[wpforms id="87"]	'); ?>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php get_footer(); ?>
