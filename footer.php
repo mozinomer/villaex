b5d65f7db6eb5f1b7c7f290863970d3931c2fbb2
Mozin Omer <mozinomer12@gmail.com>
2019-07-17 12:33:09 +0500

    footer

--- footer.php
+++ footer.php
@@ -2,17 +2,47 @@
 <?php wp_footer();?>
 </div>
 
+
+
 <footer>
-	<script type="text/javascript">
-		// (function($){
-		// 	$('#myTab a.nav-link').on('click', function(e) {
-		// 		var id = $(this).data('id');
-		// 		console.log(id);
-		// 	});
-		// })( jQuery );
-	</script>
+	<div class="container">
+		<div class="row footer-top_row">
+			<div class="col-md-4">
+				<img height="25px"  src="<?php echo get_template_directory_uri(); ?>/includes/images/icons/footercontactus.png">
+				<span>
+					<h4>
+						Cotact Us
+					</h4>
+					<p>
+						<a href="mail:info@villaextechnologies.com">info@villaextechnologies.com</a>
+					</p>
+				</span>
+			</div>
+			<div class="col-md-4">
+				<img height="25px"  src="<?php echo get_template_directory_uri(); ?>/includes/images/icons/footercontactus.png">
+				<span>
+					<h4>
+						Cotact Us
+					</h4>
+					<p>
+						<a href="mail:info@villaextechnologies.com">info@villaextechnologies.com</a>
+					</p>
+				</span>
+			</div>
+			<div class="col-md-4">
+				<img height="25px"  src="<?php echo get_template_directory_uri(); ?>/includes/images/icons/footercontactus.png">
+				<span>
+					<h4>
+						Cotact Us
+					</h4>
+					<p>
+						<a href="mail:info@villaextechnologies.com">info@villaextechnologies.com</a>
+					</p>
+				</span>
+			</div>
+		</div>
+	</div>
 </footer>
+
 </body>
-
-
 </html>
